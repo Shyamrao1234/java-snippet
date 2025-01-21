1b180ecb707e26f895fa2e00a5fa49a97e366bd0
Shyam <shyamrao.b@reactore.com>
2025-01-21 14:18:49 +0530

    Difference between == and .equal().

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,26 @@
 package org.example;
 
+import javax.swing.*;
+
 /**
  * Created by $CapName on Jan 20, 2025.
  */
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String word1 = new String("Java");
+        String word2 = new String("Java");
+
+        System.out.println("  "+ word1 == word2); //false
+
+        System.out.println(" "+word1.equals(word2));
+
+        String word3="Java";
+        String word4="Java";
+
+        System.out.println(word3 == word4);
+
+
+//        System.out.println("Hello, World!");
     }
 }
